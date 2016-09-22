64abb83aca11d9ad07e332e704e4af02f501f75b
cam-shotter <cameronshotter@gmail.com>
2016-09-22 18:07:54 +1200

    set up basic serverside app

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about' function(req, res, next) {
+  res.render('about', { a: 'a' })
+})
+
+router.get('/blog' function(req, res, next) {
+  res.render('blog', { a: 'a' })
+})
+
+router.get('/portfolio' function(req, res, next) {
+  res.render('portfolio', { a: 'a' })
+})
+
+router.get('/contact' function(req, res, next) {
+  res.render('contact', { a: 'a' })
+})
+
+module.exports = router;
