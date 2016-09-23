ad9bac1ebad01a7741bba3880cafeed82aa53266
cam-shotter <cameronshotter@gmail.com>
2016-09-23 13:48:21 +1200

    basic handlebars files set up

--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/about' function(req, res, next) {
-  res.render('about', { a: 'a' })
+router.get('/about', function(req, res, next) {
+  res.render('about', { a: 'a' });
 })
 
-router.get('/blog' function(req, res, next) {
-  res.render('blog', { a: 'a' })
+router.get('/blog', function(req, res, next) {
+  res.render('blog', { a: 'a' });
 })
 
-router.get('/portfolio' function(req, res, next) {
-  res.render('portfolio', { a: 'a' })
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { a: 'a' });
 })
 
-router.get('/contact' function(req, res, next) {
-  res.render('contact', { a: 'a' })
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { a: 'a' });
 })
 
 module.exports = router;
